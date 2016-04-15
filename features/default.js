3507ac037fb41efda2974c815203a54ea81e75c7
Chris Nault <chrisnault@gmail.com>
2016-04-15 16:52:05 -0500

    Initial commit

---
+++ features/default.js
@@ -0,0 +1,16 @@
+// defaults for function args
+function sayHello(name = 'World') {
+	console.log('Hello ' + name + '!');
+}
+
+// sayHello('chris');
+// sayHello();
+
+// with an object
+function greetUser(user = {name: 'Anonymous'}) {
+	console.log('Hello ' + user.name + '!');
+}
+
+greetUser();
+greetUser({name: 'Chris'});
+
